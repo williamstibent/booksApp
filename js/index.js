d1ac7e6b6f288570003f1969f38070ab02b5a8bd
williamstibent <possos@unicauca.edu.co>
2018-06-21 17:44:11 -0500

    juan.clomenares changes

--- js/index.js
+++ js/index.js
@@ -5,6 +5,40 @@ $(document).ready(function () {
         })
     })
 
+    $.ajax({
+        url: './components/list.html',
+        type: 'GET',
+        dataType: 'text',
+        success: function (response) {
+            $('#books-content').html(response)
+            $.ajax({
+                url: './data/data.json',
+                type: 'GET',
+                dataType: 'json',
+                success: function (books) {
+                    debugger
+                    Array.from(books).forEach(element => {
+                        let item = $('#book').clone().appendTo('.books-content');
+                        let currentHtml = item.html();
+                        let newHtml = "";
+                        Object.keys(element).forEach(k => {
+                            newHtml = currentHtml.replace("{{" + k + "}}", element[k]);
+                            currentHtml = newHtml;
+                        });
+                        item.html(newHtml)
+                    });
+                    $('#book').remove();
+                }
+            })
+        },
+        error: function () {
+            console.log(error)
+        },
+        complete: function (xhr, status) {
+            console.log(status)
+        }
+    })
+
     $('#activator-menu').one('click', reduceSizeNav)
 
     function reduceSizeNav() {
@@ -19,11 +53,11 @@ $(document).ready(function () {
 
     function changeSizeNav(reduce) {
         var widthNav
-        if(reduce){
+        if (reduce) {
             $('#username-navbar').hide()
             widthNav = '150px'
         }
-        else{
+        else {
             $('#username-navbar').show()
             widthNav = '350px'
         }
@@ -31,44 +65,4 @@ $(document).ready(function () {
             width: widthNav
         }, 500)
     }
-
-    var books = [
-        {
-            "name": "Book 1",
-            "image": "https://images-na.ssl-images-amazon.com/images/I/51HSkTKlauL._SX346_BO1,204,203,200_.jpg",
-            "description": "Harry Potter es una serie de novelas fantásticas escrita por la autora británica J. K. Rowling, considerada una de las sagas más importantes de la historia. Realmente la más leída según el Libro de los récords Guiness."
-
-        },
-        {
-            "name": "Book 2",
-            "image": "https://hpmedia.bloomsbury.com/rep/s/9781408855669_309028.jpeg",
-            "description": "Harry Potter es una serie de novelas fantásticas escrita por la autora británica J. K. Rowling, considerada una de las sagas más importantes de la historia. Realmente la más leída según el Libro de los récords Guiness."
-
-        },
-        {
-            "name": "Book 3",
-            "image": "https://hpmedia.bloomsbury.com/rep/s/9781408855683_309032.jpeg",
-            "description": "Harry Potter es una serie de novelas fantásticas escrita por la autora británica J. K. Rowling, considerada una de las sagas más importantes de la historia. Realmente la más leída según el Libro de los récords Guiness."
-
-        },
-        {
-            "name": "Book 3",
-            "image": "https://hpmedia.bloomsbury.com/rep/s/9781408855683_309032.jpeg",
-            "description": "Harry Potter es una serie de novelas fantásticas escrita por la autora británica J. K. Rowling, considerada una de las sagas más importantes de la historia. Realmente la más leída según el Libro de los récords Guiness."
-
-        }
-
-    ];
-
-    books.forEach(element => {
-        let item = $('#book').clone().appendTo('.books-content');
-        let currentHtml = item.html();
-        let newHtml = "";
-        Object.keys(element).forEach(k => {
-            newHtml = currentHtml.replace("{{" + k + "}}", element[k]);
-            currentHtml = newHtml;
-        });
-        item.html(newHtml)
-    });
-    $('#book').remove();
 })
